edc98acbf6aed772f1fed4d6691c33541825d4da
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2021-04-28 17:38:56 -0400

    Create jewels-and-stones.js

---
+++ javascript/jewels-and-stones.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    let count = 0;
+    for (let i = 0; i < S.length; i++) {
+        if (J.includes(S[i])) {
+            count++;
+        }
+    }
+    
+    return count;
+};
